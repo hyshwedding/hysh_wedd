38dd39263bd50b8abce5eccefe01dd7f8494c8b8
hyshwedding <hysh1019wedding@gmail.com>
2024-08-15 04:30:21 +0900

    first commit
    first commit

---
+++ JS/map.js
@@ -0,0 +1,19 @@
+// 카카오맵
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(37.5032910260927, 127.065495897888), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+var markerPosition  = new kakao.maps.LatLng(37.5032910260927, 127.065495897888); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
